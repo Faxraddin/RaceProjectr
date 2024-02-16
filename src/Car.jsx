996db087a97bbe1f2a999e4907bcb91fa697e0e5
Faxraddin <reydivonay@gmail.com>
2024-02-16 20:18:13 +0400

    physics in car

--- src/Car.jsx
+++ src/Car.jsx
@@ -1,3 +1,4 @@
+import { useBox } from "@react-three/cannon";
 import { useLoader,useFrame } from "@react-three/fiber";
 import { useEffect,useRef } from "react";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
@@ -8,12 +9,31 @@ export default function Car() {
         '/models/car.glb'
     ).scene;
 
+    const position = [-1.5,0.5,3];
+    const width = 0.15;
+    const height = 0.7;
+    const front = 0.15;
+    const wheelRadius = 0.05;
+
+    const chassisBodyArgs = [width, height, front * 2];
+    const [chassisBody,chassisApi] = useBox(
+        ()=>({
+            args: chassisBodyArgs,
+            mass: 150,
+            position
+        }),
+        useRef(null)
+    );
+
     useEffect(()=>{
         mesh.scale.set(0.0012,0.0012,0.0012);
         mesh.children[0].position.set(-365,-18,-67);
     },[mesh]);
 
     return(
-        <primitive object={mesh} rotation-y={Math.PI}/>
+        <mesh ref={chassisBody}>
+            <meshBasicMaterial transparent={true} opacity={0.3}/>
+            <boxGeometry args={chassisBodyArgs}/>
+        </mesh>
     )
-}
+};
